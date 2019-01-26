a21e2ff1ed77a2af1e83e7bd7bf4b0114097e8ff
Patrick Maynard <patrickmaynard@gmail.com>
2019-01-26 07:09:47 -0700

    Add config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+	$sources = [
+		[
+			'url' => 'https://www.buzzfeed.com/de/jasonaleopold',
+			'must_contain_new' => 'Jason Leopold'
+		],
+		[
+			'url' => 'https://travel.state.gov/content/travel/en/international-travel/International-Travel-Country-Information-Pages/Germany.html'
+			'must_contain_new' => '*'
+		]
+	];
+
+?>
