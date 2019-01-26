51e3f096234eda819c65a20d9a735c57afea8cfb
Patrick Maynard <patrickmaynard@gmail.com>
2019-01-26 11:00:15 -0700

    Why doesn't this detect changes?

--- check.php
+++ check.php
@@ -32,7 +32,45 @@ class Checker
 		 */
 		private function isRelevantChangePresent($source)
 		{
-		    return 'stub';
+		    if ($source['must_contain_new'] === '*') {
+				    return $this->isAnyChangePresent($source);
+				} else {
+				    return $this->isSubstringChangePresent($source);
+				}
+		}
+
+		/**
+		 * Look for a specific substring in new text.
+		 */
+    private function isSubstringChangePresent($source)
+		{  
+				$fileName = md5($source['url']);
+				$oldHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+				$newHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+				
+				preg_match_all('/.{500}'. $source['must_contain_new'] . '.{500}/sU',$newHtml,$matches);
+				foreach($matches as $match){
+			      foreach($match as $submatch){
+						    if (!is_numeric($submatch)) {
+								    if (strpos($oldHtml,$submatch)  === false) {
+										    return true;
+										}
+								}
+						}
+				}
+
+				return false;
+		}
+
+    /**
+		 * Checks to see if any change is present at all.
+		 */
+    private function isAnyChangePresent($source)
+		{
+				$fileName = md5($source['url']);
+				$oldHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+				$newHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+				return ! ($oldHtml === $newHtml);
 		}
 
 		/**
@@ -68,7 +106,8 @@ class Mailer
 {
   	public function send($alerts)
 	  { 
-	      return 'stub.';
+			echo "\n(Stub) Alerts would be generated for these urls: \n";
+			var_dump($alerts);
 	  }
 }
 
@@ -77,6 +116,8 @@ $mailer = new Mailer;
 $alerts = $checker->checkAll();
 if (count($alerts) > 0) {
     $mailer->send($alerts);
+} else {
+    echo "\nNo alerts generated.\n";
 }
 
 ?>
