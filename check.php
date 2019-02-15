0f3d632ea3f0619ea6f98ff07ca51efb78448dfb
patrickmaynard <patrickmaynard@users.noreply.github.com>
2019-02-15 19:56:43 +0100

    Fix whitespace in Eclipse

--- check.php
+++ check.php
@@ -26,88 +26,88 @@ class Checker
         }
     }
 
-		/**
-		 * Compare files and return true or false
-		 */
-		private function isRelevantChangePresent($source)
-		{
-		    if ($source['must_contain_new'] === '*') {
-				    return $this->isAnyChangePresent($source);
-				} else {
-				    return $this->isSubstringChangePresent($source);
-				}
-		}
-
-		/**
-		 * Look for a specific substring in new text.
-		 */
-    private function isSubstringChangePresent($source)
-		{  
-				$fileName = md5($source['url']);
-				$oldHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
-				$newHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName);
-				
-				preg_match_all('/.{500}'. $source['must_contain_new'] . '.{500}/sU',$newHtml,$matches);
-				foreach($matches as $match){
-			      foreach($match as $submatch){
-						    if (!is_numeric($submatch)) {
-								    if (strpos($oldHtml,$submatch)  === false) {
-										    return true;
-										}
-								}
-						}
-				}
-
-				return false;
-		}
+    /**
+     * Compare files and return true or false
+     */
+    private function isRelevantChangePresent($source)
+    {
+        if ($source['must_contain_new'] === '*') {
+            return $this->isAnyChangePresent($source);
+        } else {
+            return $this->isSubstringChangePresent($source);
+        }
+    }
 
     /**
-		 * Checks to see if any change is present at all.
-		 */
+     * Look for a specific substring in new text.
+     */
+    private function isSubstringChangePresent($source)
+        {  
+            $fileName = md5($source['url']);
+            $oldHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+            $newHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+            
+            preg_match_all('/.{500}'. $source['must_contain_new'] . '.{500}/sU',$newHtml,$matches);
+            foreach($matches as $match){
+                foreach($match as $submatch){
+                    if (!is_numeric($submatch)) {
+                        if (strpos($oldHtml,$submatch)  === false) {
+                        	    return true;
+                        }
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+    /**
+     * Checks to see if any change is present at all.
+     */
     private function isAnyChangePresent($source)
-		{
-				$fileName = md5($source['url']);
-				$oldHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
-				$newHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
-				return ! ($oldHtml === $newHtml);
-		}
+    {
+        $fileName = md5($source['url']);
+        $oldHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+        $newHtml  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName);
+        return ! ($oldHtml === $newHtml);
+    }
 
-		/**
-		 * Dump html into files
-		 */ 
+    /**
+     * Dump html into files
+     */ 
     private function populateFiles($source)
-		{ 
-		    $fileName = md5($source['url']);
-		    $html = file_get_contents($source['url']);	
+    { 
+        $fileName = md5($source['url']);
+        $html = file_get_contents($source['url']);	
+        
+        if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html)){
+        	    rename(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName,
+        					__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName
+        				);
+        			file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html);	
+        	} else {
+        			file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html);	
+        			file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html);	
+        	}
+    }
 
-		    if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html)){
-				    rename(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName,
-								__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName
-							);
-						file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html);	
-				} else {
-						file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'new-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html);	
-						file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'old-snapshots' . DIRECTORY_SEPARATOR . $fileName, $html);	
-				}
-		}
-
-		/**
-		 * Load the config values
-		 */
+    /**
+     * Load the config values
+     */
     private function loadSources()
-		{
-			  include(__DIR__ . DIRECTORY_SEPARATOR . 'config.php');
+    {
+        include(__DIR__ . DIRECTORY_SEPARATOR . 'config.php');
         return $sources;
-	  }
+    }
 }
 
 class Mailer
 {
-  	public function send($alerts)
-	  { 
-			echo "\n(Stub) Alerts would be generated for these urls: \n";
-			var_dump($alerts);
-	  }
+    public function send($alerts)
+    { 
+        echo "\n(Stub) Alerts would be generated for these urls: \n";
+        var_dump($alerts);
+    }
 }
 
 $checker = new Checker;
