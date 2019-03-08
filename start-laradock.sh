4f2e172666e3d8f1d59aecc2b0eb75c75b4ca4a0
patrickmaynard <patrickmaynard@users.noreply.github.com>
2019-03-08 18:34:31 +0100

    Fix laradock installer

--- start-laradock.sh
+++ start-laradock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/Laradock/laradock.git . \
+git clone https://github.com/Laradock/laradock.git \
+	&& cd laradock \
  	&& cp env-example .env \
 	&& docker-compose up -d nginx mysql phpmyadmin redis workspace
